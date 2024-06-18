a6a560c6cecfb7c04642fa50a2b06d8e1d19d2c7
simone menichetti <simon3menichettui95@gmail.com>
2024-06-18 14:29:52 +0200

    BUILD:( add folder Bonus and initialize miles)

---
+++ BONUS/mainjavascript/mainjavascript.js
@@ -0,0 +1,86 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+    // Creare l’array di oggetti con le informazioni fornite.
+
+                        //         MILESTONE 0:
+    // creazione dell'array di oggetti per i membri del team 
+    const membriTeam = [
+        {
+            nome: "Wayne Barnet",
+            ruolo: "Founder & Ceo",
+            foto: "wayne-barnet-founder-ceo.jpg"
+        },
+
+        {
+            nome: "Angela Caroll",
+            ruolo: "Chief Editor",
+            foto: "angela-caroll-chief-editor.jpg"
+        },
+
+        {
+            nome: "Walter Gordon",
+            ruolo: "Office Manager",
+            foto: "walter-gordon-office-manager.jpg"
+        },
+
+        {
+            nome: "Angela Lopez",
+            ruolo: "Social Media Manager",
+            foto: "angela-lopez-social-media-manager.jpg"
+        },
+
+        {
+            nome: "Scott Estrada",
+            ruolo: "Developer",
+            foto: "scott-estrada-developer.jpg"
+        },
+
+        {
+            nome: "Barbara Ramos",
+            ruolo: "Graphic Designer",
+            foto: "barbara-ramos-graphic-designer.jpg"
+        },
+    ];
+        // verifica oggetti inseriti nell'array
+            console.log(membriTeam);
+
+                        //         MILESTONE 1:
+
+    // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
+    // utilizziamo un ciclo for per iterare nell'array creato in precedenza per i membri e stampare le informazioni di ogni membro del team 
+
+    // [* miles 2 p1]
+        const teamContainer = document.querySelector('.team-container');
+
+        for(let i = 0 ; i< membriTeam.length;i++) {
+            // verifichiamo in console le informazioni per ogni membro
+            // console.log(`nome: ${membriTeam[i].nome}`);
+            // console.log(`ruolo: ${membriTeam[i].ruolo}`);
+            // console.log(`foto: ${membriTeam[i].foto}`);
+
+            // [* miles 2 p2]
+                const elementoMembro= document.createElement('div');
+                elementoMembro.innerHTML = `
+                <span>Nome: ${membriTeam[i].nome}</span>
+                <span>Ruolo: ${membriTeam[i].ruolo}</span>
+                <span>Foto: ${membriTeam[i].foto}</span>`;
+                
+                // [* miles 2 p3]
+                teamContainer.append(elementoMembro);
+        }
+        
+
+
+                    //         MILESTONE 2:
+        // Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+                                    // passaggi
+
+            // creazione di un elemento di riferimento nell html e richiamiamolo prima del ciclo for [* miles 2 p1]
+
+            // creazione degli elementi da inserire nel dom all'interno del team container  [* miles 2 p2]
+
+            // Stampare le stesse informazioni su DOM sottoforma di stringhe [* miles 2 p end]
