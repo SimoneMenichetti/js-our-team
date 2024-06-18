704dec8c3bd4035ab28ca66589ccaa830f482069
simone menichetti <simon3menichettui95@gmail.com>
2024-06-18 13:50:45 +0200

    build:(add files and folder to initialize ) feat:( JS create objets array for teammember)

---
+++ mainjavascript/mainjavascript.js
@@ -0,0 +1,46 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+    // Creare l’array di oggetti con le informazioni fornite.
+
+    // creazione dell'array di oggetti per i membri del team 
+    const membriTeam = [
+        {
+            nome: "Wayne Barnet",
+            ruolo: "Founder & Ceo",
+            foto: "wayne-barnet-founder-ceo.jpg"
+        },
+
+        {
+            nome: "Angela Caroll",
+            ruolo: "Chief Editor",
+            foto: "angela-caroll-chief-editor.jpg"
+        },
+
+        {
+            nome: "Walter Gordon",
+            ruolo: "Office Manager",
+            foto: "walter-gordon-office-manager.jpg"
+        },
+
+        {
+            nome: "Angela Lopez",
+            ruolo: "Social Media Manager",
+            foto: "angela-lopez-social-media-manager.jpg"
+        },
+
+        {
+            nome: "Scott Estrada",
+            ruolo: "Developer",
+            foto: "scott-estrada-developer.jpg"
+        },
+
+        {
+            nome: "Barbara Ramos",
+            ruolo: "Graphic Designer",
+            foto: "barbara-ramos-graphic-designer.jpg"
+        },
+
+      
+      
+    ];
