782739d7962de9d944b2255f0cfa210d5698a0dc
simone menichetti <simon3menichettui95@gmail.com>
2024-06-18 14:06:02 +0200

    feat:( add a "for" loop to iterate through the array for each member's information and verify in console)

--- mainjavascript/mainjavascript.js
+++ mainjavascript/mainjavascript.js
@@ -3,6 +3,7 @@
 // Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
     // Creare l’array di oggetti con le informazioni fornite.
 
+                        //         MILESTONE 0:
     // creazione dell'array di oggetti per i membri del team 
     const membriTeam = [
         {
@@ -40,7 +41,26 @@
             ruolo: "Graphic Designer",
             foto: "barbara-ramos-graphic-designer.jpg"
         },
-
-      
-      
     ];
+        // verifica oggetti inseriti nell'array
+            console.log(membriTeam);
+
+                        //         MILESTONE 1:
+
+    // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
+    // utilizziamo un ciclo for per iterare nell'array creato in precedenza per i membri e stampare le informazioni di ogni membro del team 
+
+        for(let i = 0 ; i< membriTeam.length;i++) {
+            // verifichiamo in console le informazioni per ogni membro
+            console.log(`nome: ${membriTeam[i].nome}`);
+            console.log(`ruolo: ${membriTeam[i].ruolo}`);
+            console.log(`foto: ${membriTeam[i].foto}`);
+        }
+
+
+                    //         MILESTONE 2:
+        // Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+            
